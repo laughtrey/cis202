5ee08f7cab0836ef8e0bdc7444bccf4767b09e94
laughtrey <laughtrey@gmail.com>
2019-04-10 19:03:53 -0700

    Fixes to getline usage.

--- -- Midterm/main.cpp
+++ -- Midterm/main.cpp
@@ -38,33 +38,33 @@ int main(int argc, char const *argv[])
 		}else if (answer == 2) {
 			std::cout << "Who would you like to search for?" << '\n';
 			std::string name;
-			std::cin >> name;
-			MyBook.search_the_address_book(name);
 			std::cin.ignore();
+			std::getline(std::cin, name);
+			MyBook.search_the_address_book(name);
 		}else if (answer == 3) {
 			std::cout << "Whose name or address would you like to change?" << '\n';
-			std::cin.ignore();
 			std::string name;
+			std::cin.ignore();
 			std::getline(std::cin, name);
 			std::cout << "Would you like to change their (1)name or (2)address?" << '\n';
 			int answer;
 			std::cin >> answer;
 			if (answer == 1) {
 				std::cout << "What do you want to change their name to?" << '\n';
-				std::cin.ignore();
 				std::string new_name;
+				std::cin.ignore();
 				std::getline(std::cin, new_name);
 				MyBook.change_the_address_book_name(name, new_name);
 			}else if (answer == 2) {
 				std::cout << "What do you want to change their address to?" << '\n';
-				std::cin.ignore();
 				std::string new_address;
+				std::cin.ignore();
 				std::getline(std::cin, new_address);
 				MyBook.change_the_address_book_address(name, new_address);
 			}
 		}else if (answer == 4) {
 			MyBook.save_the_address_book();
-		}else  {
+		}else {
 			menu = false;
 		}
 	}
