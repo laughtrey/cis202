d82b52b88bb581b8d3d82d9de373579b3b7e0974
laughtrey <laughtrey@gmail.com>
2019-04-20 03:21:59 -0700

    Sieve of Eratosthenes

--- 13 Sets/main.cpp
+++ 13 Sets/main.cpp
@@ -1,33 +1,44 @@
 /*
-File: main.cpp
-Description:
-Author: Raymond Laughrey
-Email: raymonl4963@student.vvc.edu
-Date of Creation:
-*/
-#include<iostream>
-#include<set>
+   File: main.cpp
+   Description:
+   Author: Raymond Laughrey
+   Email: raymonl4963@student.vvc.edu
+   Date of Creation:
+ */
+#include <iostream>
+#include <set>
+#include <cmath>
 void sieve_of_Eratosthenes(int n);
 int main()
 {
-  int n = 100;
-  //std::cout << sieve_of_Eratosthenes(n) << '\n';
-  sieve_of_Eratosthenes(n);
-  return 0;
+  int n;
+  std::cout << "Up to what number do you want to know all prime numbers?" << '\n';
+  std::cin >> n;
+	sieve_of_Eratosthenes(n);
+	return 0;
 }
 void sieve_of_Eratosthenes(int n)
 {
-  std::set<int> sieve;
-  std::set<int>::iterator pos;
-  for (int i = 2; i <= n; i++) {
-    sieve.insert(i);
-  }
-  for (pos = sieve.begin(); pos != sieve.end(); pos++){
-    if (*pos % 2 || *pos % 3){
-      sieve.erase(pos);
-    }
-  }
-  for (pos = sieve.begin(); pos != sieve.end(); pos++) {
-    std::cout << *pos << '\n';
-  }
+	std::set<int> sieve;
+	std::set<int>::iterator pos = sieve.begin();
+	for (int i = 2; i <= n; i++) {
+		sieve.insert(i);
+		if (pos != sieve.end())
+			pos++;
+	}
+	pos = sieve.begin();
+	while (pos != sieve.end()) {
+		int current = *pos++;
+		int remove = current + current;
+		while (remove <= n) {
+			sieve.erase(remove);
+			remove += current;
+		}
+	}
+	pos = sieve.begin();
+	while (pos != sieve.end()) {
+		std::cout << *pos << '\n';
+		pos++;
+	}
+std::cout << '\n';
 }
